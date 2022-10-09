4db063441be420bfbf9e050e0ce31f47f3e242a1
Daniel Marcano <dmarcanor2@gmail.com>
2022-10-09 17:28:31 -0400

    migration for cars table

---
+++ database/migrations/2022_10_09_205730_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        if (! Schema::hasTable('cars')) {
+            Schema::create('cars', function (Blueprint $table) {
+                $table->uuid('id');
+                $table->string('code');
+                $table->string('brand');
+                $table->string('model');
+                $table->string('color');
+                $table->string('fuel');
+                $table->string('gearbox');
+                $table->decimal('kilometer');
+                $table->decimal('price');
+                $table->string('type');
+                $table->year('year');
+                $table->string('status');
+                $table->dateTime('createdAt');
+                $table->dateTime('updatedtedAt');
+    
+                $table->primary('id');
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
